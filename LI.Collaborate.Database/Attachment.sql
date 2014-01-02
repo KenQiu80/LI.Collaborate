43fc09297f260d42ff65696cd08f970c86c94dd4
KenQiu <ken.qiu@outlook.com>
2014-01-02 13:51:34 +0800

    Sync sql & csproj & sqlproj files

---
+++ LI.Collaborate.Database/Attachment.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Attachment]
+(
+	[Id] VARCHAR(36) NOT NULL PRIMARY KEY, 
+    [EventId] VARCHAR(36) NULL, 
+    [MD5] VARCHAR(MAX) NULL, 
+    [Filename] NVARCHAR(MAX) NULL,
+    [CreatorId] VARCHAR(36) NOT NULL, 
+    [CreatorName] NVARCHAR(MAX) NOT NULL, 
+    [CreatedTime] DATETIME NOT NULL, 
+    [ModifierId] VARCHAR(36) NOT NULL, 
+    [ModifierName] NVARCHAR(MAX) NOT NULL, 
+    [ModifiedTime] DATETIME NOT NULL
+)
---
+++ LI.Collaborate.Database/Link.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Link]
+(
+	[Id] VARCHAR(36) NOT NULL PRIMARY KEY, 
+    [EventId1] VARCHAR(36) NOT NULL, 
+    [EventName1] NVARCHAR(MAX) NOT NULL, 
+    [EventId2] VARCHAR(36) NOT NULL, 
+    [EventName2] NVARCHAR(MAX) NOT NULL, 
+    [Type] INT NOT NULL, 
+    [CreatorId] VARCHAR(36) NOT NULL, 
+    [CreatorName] NVARCHAR(MAX) NOT NULL, 
+    [CreatedTime] DATETIME NOT NULL, 
+    [ModifierId] VARCHAR(36) NOT NULL, 
+    [ModifierName] NVARCHAR(MAX) NOT NULL, 
+    [ModifierTime] DATETIME NOT NULL
+)
